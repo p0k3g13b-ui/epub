a7e5af31c9129fe4facc9b13e7f204327542e49d
p0k3g13b-ui <p0k3g13b@gmail.com>
2025-12-21 20:00:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const epubListEl = document.getElementById('epub-list');
 
 // Liste des EPUB disponibles
-const epubs = ["Le parfum.epub","Entre deux mondes.epub"]; // ajoute d’autres fichiers ici
+const epubs = ["Le parfum.epub","Entre deux mondes.epub","L'ame du mal.epub"]; // ajoute d’autres fichiers ici
 
 epubs.forEach(async (name) => {
   const container = document.createElement('div');
@@ -21,5 +21,6 @@ epubs.forEach(async (name) => {
 
   epubListEl.appendChild(container);
 });
+
 
 
